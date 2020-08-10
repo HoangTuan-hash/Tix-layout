607d9c54c95dd8ef9075daf93cbad130f6d449fa
HoangTuan-hash <hoangtuan2002@gmail.com>
2020-08-10 12:32:56 +0700

    click outside

--- js/main.js
+++ js/main.js
@@ -3,12 +3,18 @@ var dropDow = document.querySelectorAll(".ticket__dropdow");
 for (let i = 0; i < ticket.length; i++) {
   ticket[i].addEventListener("click", function () {
     var index = i;
-    console.log(index);
     dropDow[i].classList.toggle("ticket__show--drop");
     for (let j = 0; j < dropDow.length; j++) {
       if (j != index) {
         dropDow[j].classList.remove("ticket__show--drop");
       }
     }
+    document.addEventListener('mouseup', function(e) {
+      var ticket = document.getElementById('ticket');
+      if (!ticket.contains(e.target)) {
+        dropDow[i].classList.remove("ticket__show--drop");
+      }
+    });
   });
+  
 }
